4430427f8a9328db3f2230ad3132c778c70205ca
Remi Rousselet <darky12s@gmail.com>
2018-10-22 23:19:21 +0200

    remove unused code

--- test/stateful_provider_test.dart
+++ test/stateful_provider_test.dart
@@ -2,8 +2,6 @@ import 'package:flutter/widgets.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:provider/provider.dart';
 
-Type _typeof<T>() => T;
-
 void main() {
   test('assets', () {
     expect(() => StatefulProvider(), throwsAssertionError);
