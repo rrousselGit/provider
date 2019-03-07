285f03e590752f5bbae4d4c0bc623909f0d8966a
Remi Rousselet <darky12s@gmail.com>
2019-03-07 06:54:03 +0100

    rename providerbase

--- lib/provider.dart
+++ lib/provider.dart
@@ -1,13 +1,5 @@
 library provider;
 
 export 'src/consumer.dart';
-export 'src/listenable_provider.dart'
-    show ListenableProvider, ChangeNotifierProvider;
-export 'src/provider.dart'
-    show
-        Provider,
-        ProviderNotFoundError,
-        StatefulProvider,
-        MultiProvider,
-        ValueListenableProvider,
-        ProviderBase;
+export 'src/listenable_provider.dart';
+export 'src/provider.dart' hide debugGetProviderUpdateShouldNotify;
