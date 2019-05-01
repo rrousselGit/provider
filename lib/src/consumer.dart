154f036362cca840e6ae0020a73e39a23db2f9f8
Remi Rousselet <darky12s@gmail.com>
2019-05-02 00:21:07 +0200

    declare consumer constructor before properties

--- lib/src/consumer.dart
+++ lib/src/consumer.dart
@@ -7,19 +7,19 @@ import 'package:provider/src/provider.dart';
 /// [builder] must not be null and may be called multiple times (such as when provided value change).
 /// {@endtemplate}
 class Consumer<T> extends StatelessWidget {
-  /// {@template provider.consumer.builder}
-  /// Build a widget tree based on the value from a [Provider<T>].
-  ///
-  /// Must not be null.
-  /// {@endtemplate}
-  final Widget Function(BuildContext context, T value) builder;
-
   /// {@template provider.consumer.constructor}
   /// Consumes a [Provider<T>]
   /// {@endtemplate}
   Consumer({Key key, @required this.builder})
       : assert(builder != null),
         super(key: key);
+
+  /// {@template provider.consumer.builder}
+  /// Build a widget tree based on the value from a [Provider<T>].
+  ///
+  /// Must not be null.
+  /// {@endtemplate}
+  final Widget Function(BuildContext context, T value) builder;
 
   @override
   Widget build(BuildContext context) {
@@ -29,13 +29,13 @@ class Consumer<T> extends StatelessWidget {
 
 /// {@macro provider.consumer}
 class Consumer2<A, B> extends StatelessWidget {
-  /// {@macro provider.consumer.builder}
-  final Widget Function(BuildContext context, A value, B value2) builder;
-
   /// {@macro provider.consumer.constructor}
   Consumer2({Key key, @required this.builder})
       : assert(builder != null),
         super(key: key);
+
+  /// {@template provider.consumer.builder}
+  final Widget Function(BuildContext context, A value, B value2) builder;
 
   @override
   Widget build(BuildContext context) {
@@ -49,14 +49,14 @@ class Consumer2<A, B> extends StatelessWidget {
 
 /// {@macro provider.consumer}
 class Consumer3<A, B, C> extends StatelessWidget {
-  /// {@macro provider.consumer.builder}
-  final Widget Function(BuildContext context, A value, B value2, C value3)
-      builder;
-
   /// {@macro provider.consumer.constructor}
   Consumer3({Key key, @required this.builder})
       : assert(builder != null),
         super(key: key);
+
+  /// {@template provider.consumer.builder}
+  final Widget Function(BuildContext context, A value, B value2, C value3)
+      builder;
 
   @override
   Widget build(BuildContext context) {
@@ -71,15 +71,14 @@ class Consumer3<A, B, C> extends StatelessWidget {
 
 /// {@macro provider.consumer}
 class Consumer4<A, B, C, D> extends StatelessWidget {
-  /// {@macro provider.consumer.builder}
-  final Widget Function(
-      BuildContext context, A value, B value2, C value3, D value4) builder;
-
   /// {@macro provider.consumer.constructor}
   Consumer4({Key key, @required this.builder})
       : assert(builder != null),
         super(key: key);
 
+  /// {@template provider.consumer.builder}
+  final Widget Function(
+      BuildContext context, A value, B value2, C value3, D value4) builder;
   @override
   Widget build(BuildContext context) {
     return builder(
@@ -94,15 +93,15 @@ class Consumer4<A, B, C, D> extends StatelessWidget {
 
 /// {@macro provider.consumer}
 class Consumer5<A, B, C, D, E> extends StatelessWidget {
-  /// {@macro provider.consumer.builder}
-  final Widget Function(
-          BuildContext context, A value, B value2, C value3, D value4, E value5)
-      builder;
-
   /// {@macro provider.consumer.constructor}
   Consumer5({Key key, @required this.builder})
       : assert(builder != null),
         super(key: key);
+
+  /// {@template provider.consumer.builder}
+  final Widget Function(
+          BuildContext context, A value, B value2, C value3, D value4, E value5)
+      builder;
 
   @override
   Widget build(BuildContext context) {
@@ -119,14 +118,14 @@ class Consumer5<A, B, C, D, E> extends StatelessWidget {
 
 /// {@macro provider.consumer}
 class Consumer6<A, B, C, D, E, F> extends StatelessWidget {
-  /// {@macro provider.consumer.builder}
-  final Widget Function(BuildContext context, A value, B value2, C value3,
-      D value4, E value5, F value6) builder;
-
   /// {@macro provider.consumer.constructor}
   Consumer6({Key key, @required this.builder})
       : assert(builder != null),
         super(key: key);
+
+  /// {@template provider.consumer.builder}
+  final Widget Function(BuildContext context, A value, B value2, C value3,
+      D value4, E value5, F value6) builder;
 
   @override
   Widget build(BuildContext context) {
