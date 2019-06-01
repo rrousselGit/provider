449604b995bcd9f3f7fe181a6cf44423e3e27402
Remi Rousselet <darky12s@gmail.com>
2019-06-02 01:15:23 +0200

    ci

--- scripts/flutter_test.sh
+++ scripts/flutter_test.sh
@@ -1,9 +1,7 @@
 cd $1
-set -e # abort CI if an error happens
 flutter packages get
 flutter format --set-exit-if-changed lib test
 flutter analyze --no-current-package lib test/
 flutter test --no-pub --coverage
 # resets to the original state
-set +e
 cd -
