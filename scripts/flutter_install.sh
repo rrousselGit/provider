e0355428587e496e797ba122aa5f70143599cb9a
Remi Rousselet <darky12s@gmail.com>
2019-06-02 00:18:57 +0200

    ci

--- scripts/flutter_install.sh
+++ scripts/flutter_install.sh
@@ -2,6 +2,5 @@ cd ..
 git clone https://github.com/flutter/flutter.git -b $1
 # script shells can't export variables. Instead we print the command, and the origin shell execute the command
 # https://stackoverflow.com/questions/11076350/how-do-you-export-a-variable-through-shell-script
-echo "export PATH=$PATH:$PWD/flutter/bin"
-echo "export PATH=$PATH:$PWD/flutter/bin/cache/dart-sdk/bin"
+echo export PATH=$PATH:$PWD/flutter/bin:$PWD/flutter/bin/cache/dart-sdk/bin
 cd -
