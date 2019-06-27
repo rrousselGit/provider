f2924d54aa2a54b02b24ca5e7dd53d6212717f67
Remi Rousselet <darky12s@gmail.com>
2019-06-27 11:57:35 +0200

    Fix tests

--- packages/provider/test/readme_test.dart
+++ packages/provider/test/readme_test.dart
@@ -3,7 +3,7 @@ import 'dart:io';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  test('root/package/provider/README.md and root/README.mf are identical',
+  test('root/package/provider/README.md and root/README.md are identical',
       () async {
     final root = await File.fromUri(Uri.parse(
             '${Directory.current.parent.parent.parent.path}/README.md'))
