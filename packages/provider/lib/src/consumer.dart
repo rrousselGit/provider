0687e2cf4fac6c8cabcec7aaa15f36846683c746
Remi Rousselet <darky12s@gmail.com>
2019-07-26 19:43:42 +0200

    Consumer performance optimisation explanation

--- packages/provider/lib/src/consumer.dart
+++ packages/provider/lib/src/consumer.dart
@@ -5,10 +5,135 @@ import 'provider.dart';
 /// {@template provider.consumer}
 /// Obtain [Provider<T>] from its ancestors and pass its value to [builder].
 ///
+/// The widget [Consumer] doesn't do any fancy work. It just calls [Provider.of]
+/// in a new widget, and delegate its `build` implementation to [builder].
+///
 /// [builder] must not be null and may be called multiple times (such as when
 /// provided value change).
 ///
-/// #### Note:
+/// The wiget [Consumer] has two main purposes:
+///
+/// * It allows obtaining a value from a provider when we don't have a
+///   [BuildContext] that is a descendant of the said provider, and therefore
+///   cannot use [Provider.of].
+///
+/// Such scenario typically happens when the widget that creates the provider
+/// is also one of its consumers, like in the following example:
+///
+/// ```dart
+/// @override
+/// Widget build(BuildContext context) {
+///   return ChangeNotifierProvider(
+///     builder: (_) => Foo(),
+///     child: Text(Provider.of<Foo>(context).value),
+///   );
+/// }
+/// ```
+///
+/// This example will throw a [ProviderNotFoundError], because [Provider.of]
+/// is called with a [BuildContext] that is an ancestor of the provider.
+///
+/// Instead, we can use the [Consumer] widget, that will call [Provider.of]
+/// with its own [BuildContext].
+///
+/// Using [Consumer], the previous example will become:
+///
+/// ```dart
+/// @override
+/// Widget build(BuildContext context) {
+///   return ChangeNotifierProvider(
+///     builder: (_) => Foo(),
+///     child: Consumer<Foo>(
+///       builder: (_, foo, __) => Text(foo.value),
+///     },
+///   );
+/// }
+/// ```
+///
+/// This won't throw a [ProviderNotFoundError] and will correctly build the
+/// [Text]. It will also update the [Text] whenever the value `foo` changes.
+///
+///
+/// * It helps with performance optimisation by having more granular rebuilds.
+///
+/// Unless `listen: false` is passed to [Provider.of], it means that the widget
+/// associated to the [BuildContext] passed to [Provider.of] will rebuild
+/// whenever the obtained value changes.
+///
+/// This is the expected behavior, but sometimes it may rebuild more widgets
+/// than needed.
+///
+/// Here's an example:
+///
+/// ```dart
+///  @override
+///  Widget build(BuildContext context) {
+///    return FooWidget(
+///      child: BarWidget(
+///        bar: Provider.of<Bar>(context),
+///      ),
+///    );
+///  }
+/// ```
+///
+/// In such code, only `BarWidget` depends on the value returned by
+/// [Provider.of]. But when `Bar` changes, then both `BarWidget` _and_
+/// `FooWidget` will rebuild.
+///
+/// Ideally, only `BarWidget` should be rebuilt. And to achieve that, one
+/// solution is to use [Consumer].
+///
+/// To do so, we will wrap _only_ the widgets that depends on a provider into
+/// a [Consumer]:
+///
+/// ```dart
+///  @override
+///  Widget build(BuildContext context) {
+///    return FooWidget(
+///      child: Consumer<Bar>(
+///        builder: (_, bar, __) => BarWidget(bar: bar),
+///      ),
+///    );
+///  }
+/// ```
+///
+/// In this situation, if `Bar` were to update, only `BarWidget` would rebuild.
+///
+/// But what if it was `FooWidget` that depended on a provider? Example:
+///
+/// ```dart
+///  @override
+///  Widget build(BuildContext context) {
+///    return FooWidget(
+///      foo: Provider.of<Foo>(context),
+///      child: BarWidget(),
+///    );
+///  }
+/// ```
+///
+/// Using [Consumer], we can handle this kind of scenario using the optional
+/// [child] argument:
+///
+/// ```dart
+///  @override
+///  Widget build(BuildContext context) {
+///    return Consumer<Foo>(
+///      builder: (_, foo, child) => FooWidget(foo: foo, child: child),
+///      child: BarWidget(),
+///    );
+///  }
+/// ```
+///
+/// In that example, `BarWidget` is built outside of [builder]. Then, the
+/// `BarWidget` instance is passed to [builder] as the last parameter.
+///
+/// This means that when [builder] is called again with new values, new
+/// instance of `BarWidget` will not be recreated.
+/// This let Flutter knows that it doesn't have to rebuild `BarWidget`.
+/// Therefore in such configuration, only `FooWidget` will rebuild
+/// if `Foo` changes.
+///
+/// ## Note:
 ///
 /// The widget [Consumer] can also be used inside [MultiProvider]. To do so, it
 /// must returns the `child` passed to [builder] in the widget tree it creates.
@@ -24,10 +149,6 @@ import 'provider.dart';
 ///   ],
 /// );
 /// ```
-///
-/// ## Performance optimizations:
-///
-/// {@macro provider.consumer.child}
 /// {@endtemplate}
 class Consumer<T> extends StatelessWidget
     implements SingleChildCloneableWidget {
@@ -41,28 +162,13 @@ class Consumer<T> extends StatelessWidget
   })  : assert(builder != null),
         super(key: key);
 
-  // fork of the documentation from
-  // https://docs.flutter.io/flutter/widgets/AnimatedBuilder/child.html
   /// The child widget to pass to [builder].
-  /// {@template provider.consumer.child}
-  ///
-  /// If a builder callback's return value contains a subtree that does not
-  /// depend on the provided value, it's more efficient to build that subtree
-  /// once instead of rebuilding it on every change of the provided value.
-  ///
-  /// If the pre-built subtree is passed as the child parameter, [Consumer] will
-  /// pass it back to the builder function so that it can be incorporated into
-  /// the build.
-  ///
-  /// Using this pre-built child is entirely optional, but can improve
-  /// performance significantly in some cases and is therefore a good practice.
-  /// {@endtemplate}
   final Widget child;
 
   /// {@template provider.consumer.builder}
   /// Build a widget tree based on the value from a [Provider<T>].
   ///
-  /// Must not be null.
+  /// Must not be `null`.
   /// {@endtemplate}
   final Widget Function(BuildContext context, T value, Widget child) builder;
 
@@ -97,8 +203,6 @@ class Consumer2<A, B> extends StatelessWidget
         super(key: key);
 
   /// The child widget to pass to [builder].
-  ///
-  /// {@macro provider.consumer.child}
   final Widget child;
 
   /// {@macro provider.consumer.builder}
@@ -137,8 +241,6 @@ class Consumer3<A, B, C> extends StatelessWidget
         super(key: key);
 
   /// The child widget to pass to [builder].
-  ///
-  /// {@macro provider.consumer.child}
   final Widget child;
 
   /// {@macro provider.consumer.builder}
@@ -178,8 +280,6 @@ class Consumer4<A, B, C, D> extends StatelessWidget
         super(key: key);
 
   /// The child widget to pass to [builder].
-  ///
-  /// {@macro provider.consumer.child}
   final Widget child;
 
   /// {@macro provider.consumer.builder}
@@ -219,8 +319,6 @@ class Consumer5<A, B, C, D, E> extends StatelessWidget
         super(key: key);
 
   /// The child widget to pass to [builder].
-  ///
-  /// {@macro provider.consumer.child}
   final Widget child;
 
   /// {@macro provider.consumer.builder}
@@ -262,8 +360,6 @@ class Consumer6<A, B, C, D, E, F> extends StatelessWidget
         super(key: key);
 
   /// The child widget to pass to [builder].
-  ///
-  /// {@macro provider.consumer.child}
   final Widget child;
 
   /// {@macro provider.consumer.builder}
