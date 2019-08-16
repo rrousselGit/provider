54c652f5862a9f27c07093f4615738191db552f8
Remi Rousselet <darky12s@gmail.com>
2019-08-16 20:43:53 +0200

    document selector

--- packages/provider/lib/src/selector.dart
+++ packages/provider/lib/src/selector.dart
@@ -1,12 +1,38 @@
 import 'package:flutter/widgets.dart';
-import 'package:provider/provider.dart';
 import 'package:provider/src/provider.dart';
 
+import 'consumer.dart';
+import 'delegate_widget.dart';
+
+/// A base class for custom [Selector].
+///
+/// It works with any [InheritedWidget]. Variants like [Selector] and
+/// [Selector6] are just syntax sugar to use [Selector0] with [Provider.of].
+///
+/// But it will **not** work with values
+/// coming from anything but [InheritedWidget].
+///
+/// As such, the following:
+///
+/// ```dart
+/// T value;
+///
+/// return Selector0(
+///   selector: (_) => value,
+///   builder: ...,
+/// )
+/// ```
+///
+/// will still call `builder` again, even if `value` didn't change.
 class Selector0<T> extends StatefulWidget
     implements SingleChildCloneableWidget {
-  Selector0(
-      {Key key, @required this.builder, @required this.selector, this.child})
-      : assert(builder != null),
+  /// Both `builder` and `selector` must not be `null`.
+  Selector0({
+    Key key,
+    @required this.builder,
+    @required this.selector,
+    this.child,
+  })  : assert(builder != null),
         assert(selector != null),
         super(key: key);
 
@@ -31,15 +57,15 @@ class Selector0<T> extends StatefulWidget
 class _Selector0State<T> extends State<Selector0<T>> {
   T value;
   Widget cache;
-  ValueWidgetBuilder<T> builder;
+  Widget oldWidget;
 
   @override
   Widget build(BuildContext context) {
     final selected = widget.selector(context);
 
-    if (builder != widget.builder || selected != value) {
+    if (oldWidget != widget || selected != value) {
       value = selected;
-      builder = widget.builder;
+      oldWidget = widget;
       cache = widget.builder(
         context,
         selected,
@@ -50,7 +76,38 @@ class _Selector0State<T> extends State<Selector0<T>> {
   }
 }
 
+/// {@template provider.selector}
+/// An equivalent to [Consumer] that can filter updates by selecting a limited
+/// amount of values and prevent rebuild if they don't change.
+///
+/// [Selector] will obtain a value using [Provider.of], then pass that value
+/// to `selector`. That `selector` callback is then tasked to return an object
+/// that contains only the informations needed for `builder` to complete.
+///
+/// The object returned by `selector` should be immutable and override
+/// [operator==] such that two objects with the same content are equal, even
+/// if they are not [identical].
+///
+/// As such, to select multiple values, the easiest solution is to use a "Tuple"
+/// from [tuple](https://pub.dev/packages/tuple):
+///
+/// ```dart
+/// Selector<Foo, Tuple2<Bar, Baz>>(
+///   selector: (_, foo) => Tuple2(foo.bar, foo.baz),
+///   builder: (_, data, __) {
+///     return Text('${data.item1}  ${data.item2}');
+///   }
+/// )
+/// ```
+///
+/// In that example, `builder` will be called again only if `foo.bar` or
+/// `foo.baz` changes.
+///
+/// For generic usage informations, see [Consumer].
+///
+/// {@endtemplate}
 class Selector<A, S> extends Selector0<S> {
+  /// {@macro provider.selector}
   Selector({
     Key key,
     @required ValueWidgetBuilder<S> builder,
@@ -65,7 +122,9 @@ class Selector<A, S> extends Selector0<S> {
         );
 }
 
+/// {@macro provider.selector}
 class Selector2<A, B, S> extends Selector0<S> {
+  /// {@macro provider.selector}
   Selector2({
     Key key,
     @required ValueWidgetBuilder<S> builder,
@@ -84,7 +143,9 @@ class Selector2<A, B, S> extends Selector0<S> {
         );
 }
 
+/// {@macro provider.selector}
 class Selector3<A, B, C, S> extends Selector0<S> {
+  /// {@macro provider.selector}
   Selector3({
     Key key,
     @required ValueWidgetBuilder<S> builder,
@@ -104,7 +165,9 @@ class Selector3<A, B, C, S> extends Selector0<S> {
         );
 }
 
+/// {@macro provider.selector}
 class Selector4<A, B, C, D, S> extends Selector0<S> {
+  /// {@macro provider.selector}
   Selector4({
     Key key,
     @required ValueWidgetBuilder<S> builder,
@@ -125,7 +188,9 @@ class Selector4<A, B, C, D, S> extends Selector0<S> {
         );
 }
 
+/// {@macro provider.selector}
 class Selector5<A, B, C, D, E, S> extends Selector0<S> {
+  /// {@macro provider.selector}
   Selector5({
     Key key,
     @required ValueWidgetBuilder<S> builder,
@@ -147,7 +212,9 @@ class Selector5<A, B, C, D, E, S> extends Selector0<S> {
         );
 }
 
+/// {@macro provider.selector}
 class Selector6<A, B, C, D, E, F, S> extends Selector0<S> {
+  /// {@macro provider.selector}
   Selector6({
     Key key,
     @required ValueWidgetBuilder<S> builder,
