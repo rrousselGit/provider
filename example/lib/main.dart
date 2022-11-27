448bd1ffee52547baceeef5803d6bdac3a16a474
Remi Rousselet <darky12s@gmail.com>
2022-11-27 15:53:54 +0100

    Fix CI

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -93,10 +93,10 @@ class Count extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Text(
-
-        /// Calls `context.watch` to make [Count] rebuild when [Counter] changes.
-        '${context.watch<Counter>().count}',
-        key: const Key('counterState'),
-        style: Theme.of(context).textTheme.headline4);
+      /// Calls `context.watch` to make [Count] rebuild when [Counter] changes.
+      '${context.watch<Counter>().count}',
+      key: const Key('counterState'),
+      style: Theme.of(context).textTheme.headlineMedium,
+    );
   }
 }
