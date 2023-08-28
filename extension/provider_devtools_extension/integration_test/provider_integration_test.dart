b9aaf9151db83baa8d53d8f110c73e61b21238d3
Kenzie Schmoll <kenzieschmoll@google.com>
2023-08-28 15:07:27 -0700

    Fixes

---
+++ extension/provider_devtools_extension/integration_test/provider_integration_test.dart
@@ -0,0 +1,3 @@
+// TODO: write integration test as proper integration test.
+// See legacy test here:
+// https://github.com/flutter/devtools/blob/master/packages/devtools_app/test/provider/provider_screen_integration_test.dart
--- extension/provider_devtools_extension/test_driver/integration_test.dart
+++ extension/provider_devtools_extension/test_driver/integration_test.dart
@@ -1,7 +1,3 @@
-// Copyright 2022 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
 import 'package:flutter_driver/flutter_driver.dart';
 import 'package:integration_test/integration_test_driver_extended.dart';
 
