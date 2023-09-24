0b86217cbcdbd302508315bdef88d5dc630bf72d
Remi Rousselet <darky12s@gmail.com>
2023-09-24 23:06:27 +0200

    Add build script

---
+++ publish.sh
@@ -0,0 +1,14 @@
+pushd packages/provider
+
+mkdir extension/devtools/build
+
+popd
+
+pushd packages/provider_devtools_extension
+
+flutter pub get &&
+dart run devtools_extensions build_and_copy \
+  --source=. \
+  --dest=../provider/extension/devtools
+
+popd
